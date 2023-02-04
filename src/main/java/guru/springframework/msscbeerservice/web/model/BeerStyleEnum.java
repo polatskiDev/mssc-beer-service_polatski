58a5c9cadec42cc9f482c0ad035d0ad095ccfe60
Orhan Polat <orhanpolat@gmail.com>
2023-02-04 10:21:52 +0100

    course classes

---
+++ src/main/java/guru/springframework/msscbeerservice/web/model/BeerStyleEnum.java
@@ -0,0 +1,9 @@
+package guru.springframework.msscbeerservice.web.model;
+
+/**
+ * @author Orhan Polat
+ */
+public enum BeerStyleEnum {
+
+    LAGER, PILSNER, STOUT, GOSE, PORTER, ALE, WHEAT, IPA, PALE_ALE, SAISON
+}
